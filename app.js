e4c85dde64a81baa4d078aee64d7e9c2743519f9
Colby Somerville <colby.somerville@gmail.com>
2015-06-16 10:56:18 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+  fs.readFile('./index.html', 'utf8', function(err, page){
+    res.send(page)
+  })
+})
+
+app.listen(3000, function(){
+  console.log("servering")
+})
